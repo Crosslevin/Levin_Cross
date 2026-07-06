77d495a7a1c34e887f505b2dff833b43a21bf75e
Crosslevin <hellolevincross@gmail.com>
2026-07-06 22:25:51 +0530

    Initialize Firebase SDK and configuration

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,18 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAP-Ts_3DtOKjEyAJMhL4uzkzjfE6vGEMg",
+  authDomain: "levin-cross.firebaseapp.com",
+  projectId: "levin-cross",
+  storageBucket: "levin-cross.firebasestorage.app",
+  messagingSenderId: "973580087814",
+  appId: "1:973580087814:web:dca38c26b8fbbbff198987",
+  measurementId: "G-SC5GY5HLY9"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+console.log("Firebase Connected Successfully!");
